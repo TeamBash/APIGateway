2d331074a50a6dc3f9d26f242f5d534301b67507
rraavi <raghuveer.raavi@gmail.com>
2016-11-26 17:01:15 -0500

    Added code to select available instance

---
+++ core/java/apigateway/src/main/java/iu/edu/teambash/GetZooNode.java
@@ -0,0 +1,49 @@
+package iu.edu.teambash;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Raghuveer Raavi on 11/26/2016.
+ */
+public class GetZooNode {
+    private static String availableIPAddress = null;
+    public GetZooNode(){
+        List<String> ipAddresses = new LinkedList<>();
+        ipAddresses.add("ec2-52-15-86-225.us-east-2.compute.amazonaws.com");
+        ipAddresses.add("ec2-52-15-134-78.us-east-2.compute.amazonaws.com");
+        ipAddresses.add("ec2-54-183-208-134.us-west-1.compute.amazonaws.com");
+        //URI uri = null;
+        //String response = null;
+        int count = 0;
+        for(String ip : ipAddresses){
+            count++;
+            try{
+                if(InetAddress.getByName(ip).isReachable(3000)) {
+                    availableIPAddress = ip;
+                    break;
+                }
+            }
+            catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(count == ipAddresses.size() && availableIPAddress == null){
+            try{
+                throw new NoZooKeeperServerUpException();
+            } catch (NoZooKeeperServerUpException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getNodeAddress(){
+        return availableIPAddress;
+    }
+}
