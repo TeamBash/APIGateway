aede7e0ae9df405965d95edfcce67c68a11da13b
rraavi <raghuveer.raavi@gmail.com>
2016-11-26 17:02:42 -0500

    Added Manager/Worker code for DataIngestor

--- core/java/apigateway/src/main/java/iu/edu/teambash/resources/DataIngestorResource.java
+++ core/java/apigateway/src/main/java/iu/edu/teambash/resources/DataIngestorResource.java
@@ -1,7 +1,14 @@
 package iu.edu.teambash.resources;
 
 import com.codahale.metrics.annotation.Timed;
-import iu.edu.teambash.StringConstants;
+import iu.edu.teambash.GetZooNode;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.discovery.ServiceDiscovery;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.ServiceProvider;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HttpMethod;
@@ -11,26 +18,86 @@ import javax.ws.rs.container.ResourceContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * Created by janakbhalla on 17/09/16.
  */
 
-
 @Path("/dataIngestor/{uid}/{year}/{month}/{date}/{station}")
 public class DataIngestorResource extends AbstractResource {
+
+    private static int index = 0;
+    String availableIPAddress = null;
+
+    private static int getIndex(){
+        return index;
+    }
+
+    private static void setIndex(int index){
+        DataIngestorResource.index = index;
+    }
 
     @Context
     private ResourceContext rc;
 
     public DataIngestorResource() {
+        GetZooNode node = new GetZooNode();
+        availableIPAddress = node.getNodeAddress();
+    }
+
+    public String delegate(){
+        System.out.println("System starting to delegate: ");
+
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(availableIPAddress+":2181", new RetryNTimes(5,1000));
+        curatorFramework.start();
+
+        ServiceDiscovery<Void> serviceDiscovery = ServiceDiscoveryBuilder.builder(Void.class).basePath("services").client(curatorFramework).build();
+
+        try {
+            serviceDiscovery.start();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ServiceProvider<Void> serviceProvider = serviceDiscovery.serviceProviderBuilder().serviceName("dataIngestor").build();
+
+        try {
+            serviceProvider.start();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try{
+            List<ServiceInstance<Void>> instances = (List<ServiceInstance<Void>>) serviceProvider.getAllInstances();
+            if(instances.size() == 0) {
+                System.out.println("No instances found for this service");
+                return "No instances found for this service";
+            }
+//            System.out.println(instances);
+            int currentIndex = DataIngestorResource.getIndex();
+            DataIngestorResource.setIndex(currentIndex+1);
+
+            String address = instances.get(currentIndex%instances.size()).getUriSpec().getParts().get(0).getValue();
+//            System.out.println(address);
+            return address;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "Done!";
     }
 
     @GET
     @Timed
     public Response redirect(@PathParam("uid") int uid, @PathParam("year") String year, @PathParam("month") String month, @PathParam("date") String date, @PathParam("station") String station) {
 
-        Response response = invokeRemoteService(1, uid, StringConstants.DATA_INGESTOR + year + "/" + month + "/" + date + "/" + station, MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON, HttpMethod.GET, null);
+        String dataIngestorAddress = this.delegate();
+        System.out.println(dataIngestorAddress);
+        Response response = invokeRemoteService(1, uid, dataIngestorAddress + year + "/" + month + "/" + date + "/" + station, MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON, HttpMethod.GET, null);
 
         String url = response.readEntity(String.class);
         StormDetectionResource stormDetectionResource = rc.getResource(StormDetectionResource.class);
