41800bbb3a5fed5b307673d27fae6adde3d75be3
Janak Bhalla <janak1710@gmail.com>
2016-12-11 20:10:07 -0500

    Restart Job Feature

--- core/java/apigateway/src/main/java/iu/edu/teambash/resources/JobsResource.java
+++ core/java/apigateway/src/main/java/iu/edu/teambash/resources/JobsResource.java
@@ -3,10 +3,7 @@ package iu.edu.teambash.resources;
 import iu.edu.teambash.GetZooNode;
 import org.apache.curator.x.discovery.ServiceProvider;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.HttpMethod;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.container.ResourceContext;
 import javax.ws.rs.core.Context;
@@ -18,7 +15,8 @@ import java.util.List;
 /**
  * Created by janakbhalla on 11/12/16.
  */
-@Path("/getJobDetails/{uid}")
+
+@Path("/jobsapi")
 public class JobsResource extends AbstractResource {
 
     private static int index = 0;
@@ -33,6 +31,7 @@ public class JobsResource extends AbstractResource {
 
 
     @GET
+    @Path("/getJobDetails/{uid}")
     public Response getJobDetails(@PathParam("uid") int uid){
         JobsRegistryResource jobsRegistryResource = rc.getResource(JobsRegistryResource.class);
         List<String> jobNames = jobsRegistryResource.getJobNames(uid).readEntity(new GenericType<List<String>>(){});
@@ -44,6 +43,16 @@ public class JobsResource extends AbstractResource {
         return invokeService(getJobDetailsAddr, MediaType.APPLICATION_JSON,
                 MediaType.APPLICATION_JSON, HttpMethod.POST, Entity.entity(jobNames, MediaType.APPLICATION_JSON));
 
+    }
+
+    @POST
+    @Path("/restartJob/{uid}/")
+    public Response restartJob(String jobName, @PathParam("uid") int uid){
+        ServiceProvider<Void> serviceProvider = delegate(availableIPAddress, "runWeatherForecast");
+        String runWeatherForecast = getAddress(serviceProvider, index);
+        index++;
+        System.out.println(runWeatherForecast);
+        return invokeRemoteService(5, uid, runWeatherForecast, MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON, HttpMethod.POST, Entity.entity(jobName, MediaType.TEXT_PLAIN));
     }
 
 }
